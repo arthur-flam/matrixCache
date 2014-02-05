66df0914fb83ffab06374533eac2d29800e06268
Arthur Flam <arthur.flam@gmail.com>
2014-02-05 18:26:31 +0100

    Initial Commit

---
+++ projetInfo-AFMC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  projetInfo-AFMC
+//
+//  Created by Arthur Flam on 05/02/2014.
+//  Copyright (c) 2014 AFlam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

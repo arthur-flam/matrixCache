76a51c11372bf5b550fa96e10b6e3a33c70b4fcc
Arthur Flam <arthur.flam@gmail.com>
2014-02-06 12:57:30 +0100

    more or less Q27

---
+++ Cache.cpp
@@ -0,0 +1,9 @@
+//
+//  Cache.cpp
+//  projetInfo-AFMC
+//
+//  Created by Arthur Flam on 06/02/2014.
+//  Copyright (c) 2014 AFlam. All rights reserved.
+//
+
+#include "Cache.h"
---
+++ Cache.h
@@ -0,0 +1,14 @@
+//
+//  Cache.h
+//  projetInfo-AFMC
+//
+//  Created by Arthur Flam on 06/02/2014.
+//  Copyright (c) 2014 AFlam. All rights reserved.
+//
+
+#ifndef __projetInfo_AFMC__Cache__
+#define __projetInfo_AFMC__Cache__
+
+#include <iostream>
+
+#endif /* defined(__projetInfo_AFMC__Cache__) */
